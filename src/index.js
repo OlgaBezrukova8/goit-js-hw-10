6aa9733be508f3ff8e689fa859501cac7956c7fb
OlgaBezrukova8 <olgabezrukova888@gmail.com>
2022-06-09 22:36:15 +0100

    Added full resolve task

--- src/index.js
+++ src/index.js
@@ -16,33 +16,51 @@ function onSearchName() {
   const currentName = refs.input.value;
   const currentNameTrim = currentName.trim();
 
+  if (currentNameTrim === '') {
+    refs.countryList.innerHTML = '';
+    return;
+  }
+
   fetchCountries(currentNameTrim)
     .then(renderCountryCard)
     .catch(() => {
       Notify.failure('Oops, there is no country with that name');
     });
-
-  if (currentNameTrim === '') {
-    refs.countryList.innerHTML = '';
-  }
 }
 
 function renderCountryCard(country) {
-  const markup = country
-    .map(({ name, capital, population, flags, languages }) => {
-      return `
-       <div class="list">
-         <h2 class="country__name"><img class="coutry__image" src=${
+  let markup = '';
+
+  if (country.length > 10) {
+    Notify.info('Too many matches found. Please enter a more specific name.');
+    return;
+  } else if (country.length >= 2 && country.length <= 10) {
+    console.log('object');
+    markup = country
+      .map(({ name, flags }) => {
+        return `
+     <div class="list">
+       <h2 class="country__name"><img class="coutry__image" src=${flags.svg} width="25">${name.official}</h2>
+     </div>`;
+      })
+      .join('');
+  } else {
+    markup = country
+      .map(({ name, capital, population, flags, languages }) => {
+        return `
+       <div class="list-first">
+         <h2 class="country-first__name"><img class="coutry__image" src=${
            flags.svg
          } width="25">${name.official}</h2>
-         <p class="country__text country__capital"><span class="text">Capital: </span>${capital}</p>
-         <p class="country__text country__population"><span class="text">Population: </span>${population}</p>
-         <p class="country__text country__languages"><span class="text">Languages: </span>${Object.values(
+         <p class="country__text"><span class="text">Capital: </span>${capital}</p>
+         <p class="country__text"><span class="text">Population: </span>${population}</p>
+         <p class="country__text"><span class="text">Languages: </span>${Object.values(
            languages
          )}</p>
        </div>`;
-    })
-    .join('');
+      })
+      .join('');
+  }
 
   refs.countryList.innerHTML = markup;
   return markup;
